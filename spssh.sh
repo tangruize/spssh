1e63aada1af2fddcdefd7c52faf511ddd78064a3
Ruize Tang <1466040111@qq.com>
2020-09-22 22:43:58 +0800

    exit when all ssh disconnected

--- spssh.sh
+++ spssh.sh
@@ -16,20 +16,22 @@ export TMPDIR=`mktemp -d -p /tmp`
 TMPFILE=`mktemp`
 bash -c 'kill -STOP $$' &
 EXITPID=$!
+KILLCAT="pkill -g 0 -x cat"
+KILLEXIT="find $TMPDIR -type p -exec false {} + && kill -INT -- -$$"
 
 while test "$#" -gt 0; do
     TMPFIFO=`mktemp -u`
     mkfifo $TMPFIFO
-    CMD="bash -c 'tail -F $TMPFILE --pid $EXITPID >> $TMPFIFO && pkill -g 0 cat & stty -echo -echoctl raw; ssh -tt $1 < $TMPFIFO & cat >> $TMPFIFO'"
+    CMD="bash -c 'stty -echo -echoctl raw; (tail -F $TMPFILE --pid $EXITPID >> $TMPFIFO; $KILLCAT) & (ssh -tt $1 < $TMPFIFO; $KILLCAT) & (cat >> $TMPFIFO; rm -f $TMPFIFO; $KILLEXIT)'"
     shift
     if test "$XTERM" = "/usr/bin/gnome-terminal"; then
         eval $XTERM -- $CMD &
     else
-        $XTERM -e "$CMD" &
+        $XTERM -e "$CMD" 2> /dev/null &
     fi
 done
 
-trap 'echo Quitting ...; pkill -g 0 cat; trap "" 2 15' 2 15
+trap "echo Quitting ...; $KILLCAT; trap '' 2 15" 2 15
 echo Run commands on all servers, Ctrl + D to exit:
 stty intr ^D
 cat >> $TMPFILE
